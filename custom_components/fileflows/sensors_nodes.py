e81f78623bca4a3c1fea1dfded68adf62617e471
Deosrc <deosrc@g-de.co.uk>
2023-10-05 18:24:48 +0100

    Add node architecture sensor

--- custom_components/fileflows/sensors_nodes.py
+++ custom_components/fileflows/sensors_nodes.py
@@ -12,6 +12,8 @@ async def async_setup_entry(hass, entry, async_add_devices):
     for node in node_info_coordinator.data:
         async_add_devices([
             OperatingSystemNodeSensor(node_info_coordinator, entry, node["Uid"])
+            OperatingSystemNodeSensor(node_info_coordinator, entry, node["Uid"]),
+            ArchitectureNodeSensor(node_info_coordinator, entry, node["Uid"])
         ])
 
 
@@ -51,4 +53,43 @@ class OperatingSystemNodeSensor(NodeEntity, SensorEntity):
             return "mdi:linux"
         if self.__raw_value == 3:
             return "mdi:apple"
+        return "mdi:desktop-classic"
+
+
+class ArchitectureNodeSensor(NodeEntity, SensorEntity):
+
+    _attr_has_entity_name = True
+    _attr_name = "Architecture"
+    _attr_entity_category = EntityCategory.DIAGNOSTIC
+
+    @property
+    def unique_id(self):
+        return f"{self._unique_id_prefix}_architecture"
+
+    @property
+    def __raw_value(self):
+        return int(self._data.get("Architecture"))
+
+    @property
+    def native_value(self) -> str | None:
+        if self.__raw_value == 1:
+            return "x86"
+        if self.__raw_value == 2:
+            return "x64"
+        if self.__raw_value == 3:
+            return "Arm32"
+        if self.__raw_value == 4:
+            return "Arm64"
+        return "Unknown"
+
+    @property
+    def icon(self) -> str | None:
+        if self.__raw_value == 1:
+            return "mdi:cpu-32-bit"
+        if self.__raw_value == 2:
+            return "mdi:cpu-64-bit"
+        if self.__raw_value == 3:
+            return "mdi:cpu-32-bit"
+        if self.__raw_value == 4:
+            return "mdi:cpu-64-bit"
         return "mdi:desktop-classic"
