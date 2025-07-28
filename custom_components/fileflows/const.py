1199ff655965ca4da5e6261957b85906de0ee13d
wildenrou <wildenrou@gmail.com>
2025-07-28 12:34:24 -0400

    Update const.py

--- custom_components/fileflows/const.py
+++ custom_components/fileflows/const.py
@@ -4,10 +4,12 @@ DOMAIN = "fileflows"
 
 # Configuration keys
 CONF_API_KEY = "api_key"
+CONF_CONNECTED_LAST_SEEN_TIMESPAN = "connected_last_seen_timespan"
 
 # Default values
 DEFAULT_PORT = 8585  # Updated to match user's FileFlows setup
 DEFAULT_SCAN_INTERVAL = 30
+DEFAULT_CONNECTED_LAST_SEEN_TIMESPAN = 5  # 5 minutes
 
 # Sensor types
 SENSOR_TYPES = {
